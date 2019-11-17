aa838c3e6d256d4b7b0866a11ba5c8b95dda2694
sst52 <saitejplacements@gmail.com>
2019-11-17 20:09:46 +0530

    Trying to runserver

--- research/calc/urls.py
+++ research/calc/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-import views
+from . import views
 
 urlpatterns = [
-	path('/', views.home, name="home") # For calling this application in home page
+	path(r'^$', views.home, name="home") # For calling this application in home page
 ]
--- research/research/urls.py
+++ research/research/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import include
+
 urlpatterns = [
+	path(r'^polls/', include('calc.urls')),
     path('admin/', admin.site.urls),
+    #path('admin/', include('calc.urls'))
 ]
